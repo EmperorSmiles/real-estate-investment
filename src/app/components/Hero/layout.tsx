a89f2deb9e76f5416595ddff1d83dad96fbe7d53
EmperorSmiles <iamfavoursmiles@gmail.com>
2023-12-20 18:24:28 +0100

    Updated

--- src/app/components/Hero/layout.tsx
+++ src/app/components/Hero/layout.tsx
@@ -1,9 +1,24 @@
-import Design from "@/app/assets/Design.png";
+import Image from 'next/image'
+import background from '../../../../public/BGimage.png'
 
 export default function Hero() {
   return (
     <>
-      <div className="bg-custom-background-image bg-cover bg-center w-screen h-80" style={{ backgroundImage: `url(${Design})` }}>This Image</div>
+      <div className="relative bg-cover bg-center h-96">
+        <Image
+          src={background}
+          alt="Hero"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-black to-black opacity-60"></div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          {/* Your content goes here */}
+          <h1 className="text-gray-200 text-4xl font-bold">Smart Homes, Smarter Investing</h1>
+        </div>
+      </div>
     </>
   )
 }
