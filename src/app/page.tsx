96d8ce0b8836c0a9deff2cd8fed9c245d4dbb586
EmperorSmiles <iamfavoursmiles@gmail.com>
2023-12-20 18:24:09 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import NavBar from './components/NavBar/layout'
+import Hero from './components/Hero/layout'
 
 export default function Home() {
   return (
-    <div className='bg-purple-400 p-5'>Welcome Home</div>
+    <div className=' '>
+      <NavBar />
+      <Hero />
+    </div>
+    
   )
 }
