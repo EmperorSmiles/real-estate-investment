150d88476fd32fb4f99a7996693a47a365940788
EmperorSmiles <iamfavoursmiles@gmail.com>
2023-12-20 18:23:54 +0100

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        <Hero />
         {children}
       </body>
     </html>
