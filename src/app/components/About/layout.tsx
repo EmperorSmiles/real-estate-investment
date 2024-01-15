ae4114649911f1f12970bc7e8ed2760d5edf1f4a
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-15 13:18:42 +0100

    Updated

---
+++ src/app/components/About/layout.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="bg-slate-200 min-h-0h-56 flex items-center justify-between space-x-6">
+            <h1 className="text-5xl m-5 font-bold text-slate-900">Why Us</h1>
+            <p className="mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, hic maiores eius voluptatem quasi tempora officia aliquam, atque porro nisi debitis provident repellat ut quo voluptatibus blanditiis eaque libero ipsa! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut distinctio voluptas, placeat eaque veniam iste quod! Explicabo obcaecati est distinctio?</p>
+    </div>
+    )
+}
