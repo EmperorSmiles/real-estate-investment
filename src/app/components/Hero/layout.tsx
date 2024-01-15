b4e5eae5cb2a71dc27813c4d16c46c293f80290a
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-15 15:06:39 +0100

    Updated

--- src/app/components/Hero/layout.tsx
+++ src/app/components/Hero/layout.tsx
@@ -40,8 +40,8 @@ export default function Hero() {
         />
         <div className="absolute inset-0 bg-gradient-to-b from-black to-black opacity-70"></div>
         <div>
-          <div className="absolute inset-0 flex items-center px-10 justify-between">
-            <h1 className="text-gray-300 text-5xl font-bold flex flex-col"><span className='px-14'>SMART </span> <span className='px-8'>HOMES, </span><span className='px-20'>SMARTER</span> <span className='px-12'>INVESTMENTS</span></h1>
+          <div className="absolute inset-0 flex items-center sm:px-2 lg:px-10 justify-between">
+            <h1 className="text-gray-300 text-2xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold flex flex-col"><span className='px-14'>SMART </span> <span className='px-8'>HOMES, </span><span className='px-20'>SMARTER</span> <span className='px-12'>INVESTMENTS</span></h1>
             <div className="relative inset-0 flex flex-col py-10 text-gray-300 ">
               {social.map((social, idx) => (
               <a href={social.url} target="_blank" key={idx} className={`${social.spacing} border-2 p-2 rounded-full border-gray-300 hover:bg-white hover:text-black hover:border-black`}>
@@ -50,7 +50,7 @@ export default function Hero() {
             ))}
           </div>
           </div>
-          <p className='text-gray-300 absolute inset-0 flex items-center justify-center top-80 '>
+          <p className='text-gray-300 absolute inset-0 flex items-center justify-center top-80 font-roboto'>
           At IntelliVest, we are your trusted partner on the path to property investment success
           </p>
         </div>
