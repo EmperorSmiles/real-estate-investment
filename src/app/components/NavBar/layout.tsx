97af92af912e502018620bfe74d384fdb428db40
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-15 13:18:52 +0100

    Updated

--- src/app/components/NavBar/layout.tsx
+++ src/app/components/NavBar/layout.tsx
@@ -2,29 +2,30 @@ import Link  from "next/link";
 
 export default function NavBar() {
     return (
-      <nav className="bg-transparent text-gray-200 font-playfair font-bold absolute inset-x-0 top-0 z-30">
-        <div className="flex justify-between p-3">
-                  <h1 className="">IntelliVest</h1>
-                  <ul className="flex space-x-7">
-                      <li>
+      <nav className="bg-transparent text-gray-300 font-open_sans absolute inset-x-0 top-0 z-30">
+        <div className="flex justify-around p-3 items-center ">
+                  <h1 className="text-lg font-bold">Intelli<span className="text-indigo-500 font-extrabold">V</span>est</h1>
+                  <ul className="flex space-x-3 text-sm ">
+                      <li className="hover:text-white">
                       <Link href="/">Home</Link>
                       </li>
-                      <li>
+                      <li className="hover:text-white">
                       <Link href="header">How it Works</Link>
                       </li>
-                      <li>
+                      <li className="hover:text-white">
                       <Link href="#">Smart Property Investing</Link>
                       </li>
-                      <li>
+                      <li className="hover:text-white">
                       <Link href="#">Market Insights</Link>
                       </li>
-                      <li>
+                      <li className="hover:text-white">
                       <Link href="#">Market Insights</Link>
                       </li>
-                      <li>
+                      <li className="hover:text-white">
                       <Link href="#">FAQs</Link>
                       </li>
           </ul>
+          <button className="bg-transparent rounded-3xl border-gray-300 border-2 px-2 hover:border-white hover:text-white">Get Started</button>
         </div>
       </nav>
     )
