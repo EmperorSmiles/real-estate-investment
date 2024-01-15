b5e954dbf7687cb1ca78e3cf03692eee1b4146c1
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-15 13:18:33 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar/layout'
 import Hero from './components/Hero/layout'
+import About from './components/About/layout'
 
 export default function Home() {
   return (
     <div className=' '>
       <NavBar />
       <Hero />
+      <About />
     </div>
     
   )
