9c6ea9fcb5b0e9d75bdfe3a73f96b8755045e54f
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-16 17:28:35 +0100

    Updated

--- src/app/components/About/layout.tsx
+++ src/app/components/About/layout.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="bg-slate-200 min-h-0h-56 flex items-center justify-between space-x-6">
             <h1 className="text-5xl m-5 font-bold text-slate-900">Why Us</h1>
-            <p className="mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, hic maiores eius voluptatem quasi tempora officia aliquam, atque porro nisi debitis provident repellat ut quo voluptatibus blanditiis eaque libero ipsa! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut distinctio voluptas, placeat eaque veniam iste quod! Explicabo obcaecati est distinctio?</p>
+            <p className="mt-6">We are your trusted partner in the world of property investment. With years of expertise and a deep understanding of the real estate market, we help you make informed investment decisions that aligns with your goals</p>
     </div>
     )
 }
