f9b10b3d017ed758817de6479f8f15accba4b04e
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-18 13:23:40 +0100

    Updated

--- src/app/components/About/layout.tsx
+++ src/app/components/About/layout.tsx
@@ -7,7 +7,7 @@ import Sujimoto from '../../../../public/Sujimoto.png'
 export default function About() {
     return (
         <>
-            <div className="bg-slate-200 min-h-56 flex items-center justify-between">
+            <div className="bg-slate-200 min-h-56 flex items-center justify-between p-4">
                 <h1 className="sm:text-sm lg:text-5xl mx-5 font-bold text-slate-900">Why Us</h1>
                 <div className='flex flex-col'>
                 <p className="mt-6 text-sm lg:text-3xl pr-2">We are your trusted partner in the world of property investment. With years of expertise and a deep understanding of the real estate market, we help you make informed investment decisions that aligns with your goals</p>
