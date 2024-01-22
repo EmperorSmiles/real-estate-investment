c1c942cb86792ac1730a7af39c935b4a9c11a192
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-22 19:58:47 +0100

    Updated

--- src/app/components/Body/layout.tsx
+++ src/app/components/Body/layout.tsx
@@ -5,7 +5,7 @@ export default function Body() {
         <div className="w-full h-64">
             <h2 className="text-lg font-bold">Affordable Property Investment in Singapore</h2>
 
-            <p className="font-open_sans italic font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab debitis voluptatibus, temporibus voluptate cum voluptatem, sit, ipsam voluptas aspernatur neque corrupti perspiciatis repellendus saepe id? Quasi nam vitae, quae veritatis eveniet sed delectus ullam earum modi. Sint eos possimus veritatis at consequuntur magnam, dicta quis.</p>
+            <p className="font-open_sans italic font-extralight">With Singapore property prices growing over 60% within the last 10 years*, the dream of owning a home continues to be a dream for many. That&apos;s why we created IntelliVest. We enable you to invest in the property market with as little as $250 in as fast as a few minutes from signup to purchase</p>
         </div>
     )
 }
