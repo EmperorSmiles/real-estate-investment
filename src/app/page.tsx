8b5edbf0759269742d123248db13aecf3a8f1141
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-22 19:47:22 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar/layout'
 import Hero from './components/Hero/layout'
 import About from './components/About/layout'
+import Body from './components/Body/layout'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <NavBar />
       <Hero />
       <About />
+      <Body />
     </div>
     
   )
