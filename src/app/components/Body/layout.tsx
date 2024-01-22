87d7b2ba0e7a48e8612050e1ff4e99c95d5d78c9
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-22 19:47:39 +0100

    Updated

---
+++ src/app/components/Body/layout.tsx
@@ -0,0 +1,11 @@
+import {  } from "module";
+
+export default function Body() {
+    return (
+        <div className="w-full h-64">
+            <h2 className="text-lg font-bold">Affordable Property Investment in Singapore</h2>
+
+            <p className="font-open_sans italic font-extralight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab debitis voluptatibus, temporibus voluptate cum voluptatem, sit, ipsam voluptas aspernatur neque corrupti perspiciatis repellendus saepe id? Quasi nam vitae, quae veritatis eveniet sed delectus ullam earum modi. Sint eos possimus veritatis at consequuntur magnam, dicta quis.</p>
+        </div>
+    )
+}
