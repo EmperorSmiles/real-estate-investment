c60f44e0b2d55ba3275163293ad07a8d716bd7ee
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-24 17:36:51 +0100

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import NavBar from './components/NavBar/layout'
-import Hero from './components/Hero/layout'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "./components/NavBar/layout";
+import Hero from "./components/Hero/layout";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'IntelliVest',
-  description: 'Smart homes, smarter investments. Welcome to IntelliVest.',
-}
+    title: "IntelliVest",
+    description: "Smart homes, smarter investments. Welcome to IntelliVest.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <NavBar />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <NavBar />
+                {children}
+            </body>
+        </html>
+    );
 }
