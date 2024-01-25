1356d1844777d75f18461d9f8f7322ff9eeab17d
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-25 17:21:00 +0100

    Updated

--- src/app/components/LatestProperty/layout.tsx
+++ src/app/components/LatestProperty/layout.tsx
@@ -1,3 +1,8 @@
 export default function LatestProperty() {
-    return <div></div>;
+    return (
+        <div className="bg-slate-200 h-full">
+            <h1>Latest Property</h1>
+            <p></p>
+        </div>
+    );
 }
