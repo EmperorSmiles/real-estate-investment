f71529351593d26c3ab205c2af2ed6cb2d30d514
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-25 18:42:25 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import NavBar from './components/NavBar/layout'
-import Hero from './components/Hero/layout'
-import About from './components/About/layout'
-import Body from './components/Body/layout'
+import NavBar from "./components/NavBar/layout";
+import Hero from "./components/Hero/layout";
+import About from "./components/About/layout";
+import Body from "./components/Body/layout";
+import Property from "./components/LatestProperty/layout";
 
 export default function Home() {
-  return (
-    <div className=' '>
-      <NavBar />
-      <Hero />
-      <About />
-      <Body />
-    </div>
-    
-  )
+    return (
+        <div className=" ">
+            <NavBar />
+            <Hero />
+            <About />
+            <Body />
+            <Property />
+        </div>
+    );
 }
