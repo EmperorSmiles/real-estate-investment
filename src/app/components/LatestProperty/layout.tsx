854b3e71d63c530f392fa313c20a0ae44f93851e
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-25 18:42:34 +0100

    Updated

--- src/app/components/LatestProperty/layout.tsx
+++ src/app/components/LatestProperty/layout.tsx
@@ -1,7 +1,7 @@
 export default function LatestProperty() {
     return (
-        <div className="bg-slate-200 h-full">
-            <h1>Latest Property</h1>
+        <div className="bg-slate-200 h-full p-8">
+            <h1 className="text-3xl font-bold font-roboto">Latest Property</h1>
             <p>
                 Explore our high-end projects tailored for both investors and
                 individuals. With a deep understanding of the Luxembourg market,
