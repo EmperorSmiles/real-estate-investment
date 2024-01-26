81ac1bf6aaf969b58dee6c32a3fbfe803ab79021
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-26 12:41:03 +0100

    Updated

--- src/app/components/LatestProperty/layout.tsx
+++ src/app/components/LatestProperty/layout.tsx
@@ -48,15 +48,15 @@ export default function LatestProperty() {
     return (
         <div className="bg-slate-200 h-full p-8">
             <h1 className="text-3xl font-bold font-roboto">Latest Property</h1>
-            <div className="flex p-8">
-                <p className="w-full">
+            <div className="flex">
+                <p className="mr-10">
                     Explore our high-end projects tailored for both investors
                     and individuals. With a deep understanding of the Luxembourg
                     market, our real estate agency has expanded its reach and is
                     proud to offer a diverse selection of quality properties,
                     catering to various tastes and budgets
                 </p>
-                <div className="flex">
+                <div className="flex h-full mt-4">
                     <button className="rounded-full border-2 border-black p-2 hover:bg-black hover:text-white">
                         <FaArrowLeft />
                     </button>
