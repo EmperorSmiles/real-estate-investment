ff024b1b4ba2e15bacc36efb3c2aa73564f43005
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-30 14:56:20 +0100

    Updated

--- src/app/components/Slider/layout.tsx
+++ src/app/components/Slider/layout.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 export default function Slider() {
     return (
-        <div className="p-4 flex-col">
-            <div className="flex gap-4 mb-4 relative transition-transform ease-in-out duration-500">
+        <div className="p-4 flex-col w-full">
+            <div className="flex gap-4 mb-4 relative transition-transform ease-in-out duration-500 overflow-auto">
                 {items.map((img, idx) => (
-                    <div key={idx}>
+                    <div key={idx} className="relative">
                         <Image
                             src={img.src}
                             alt=""
-                            width={500}
-                            height={100}
+                            width={900}
+                            height={700}
                             priority
                             className="w-full h-full object-cover"
                         />
+                        <div className="absolute inset-0 bg-gradient-to-b from-gray-600 to-gray-600 opacity-70"></div>
                     </div>
                 ))}
-                <div className="absolute inset-0 bg-gradient-to-b from-black to-black opacity-70"></div>
             </div>
         </div>
     );
