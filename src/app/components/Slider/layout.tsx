11ff0725515ad14b149fbc05a92fa01c15b54999
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-30 11:52:20 +0100

    Updated

--- src/app/components/Slider/layout.tsx
+++ src/app/components/Slider/layout.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 
 export default function Slider() {
     return (
-        <div className="p-4">
-            <div className="flex">
+        <div className="p-4 h-full">
+            <div className="flex gap-4 mb-4 relative">
                 {items.map((img, idx) => (
                     <div key={idx}>
                         <Image
                             src={img.src}
                             alt=""
-                            width={600}
+                            width={500}
                             height={500}
                             priority
                         />
                     </div>
                 ))}
+                <div></div>
             </div>
         </div>
     );
