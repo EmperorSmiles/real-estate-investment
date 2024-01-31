7619b76c526a5df87f8768187caaf9dec3caca12
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-31 16:07:46 +0100

    Updated

---
+++ src/app/components/Steps/layout.tsx
@@ -0,0 +1,14 @@
+export default function Steps() {
+    const steps = [
+        {
+            title: "Step 01",
+            desc: "Buy IntelliVest in your Currency",
+            note: "We divide properties into unique assets called IntelliVest, Each IntelliVest represents a fraction of the current value of the property",
+        },
+    ];
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
