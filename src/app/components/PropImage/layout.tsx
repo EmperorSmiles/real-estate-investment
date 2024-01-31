3201fd6d520d830e7e680bb8821309b855ee92f3
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-01-31 15:01:42 +0100

    Updated

--- src/app/components/PropImage/layout.tsx
+++ src/app/components/PropImage/layout.tsx
@@ -4,22 +4,22 @@ import Description from "../Description/layout";
 
 export default function PropImage() {
     return (
-        <div className="w-full h-full overflow-hidden p-2 grid">
+        <div className="w-full h-full overflow-auto p-2 grid">
             <div className="flex gap-6">
                 {items.map((item, idx) => (
                     <div key={idx} className="w-64 flex-shrink-0 relative">
-                        <div className="mb-2">
-                            <Image
-                                src={item.src}
-                                alt=""
-                                height={700}
-                                width={400}
-                                className="w-full h-full object-cover"
-                            />
-                        </div>
+                        <Image
+                            src={item.src}
+                            alt=""
+                            height={700}
+                            width={400}
+                            className="w-full h-full object-cover"
+                        />
+
+                        <div className="absolute inset-0 bg-gradient-to-b from-gray-600 to-gray-600 opacity-70"></div>
 
                         {item && (
-                            <div className="bg-gradient-to-b from-gray-600 to-gray-600 opacity-70 p-2 text-white">
+                            <div className="p-2 w-full">
                                 <h1 className="text-xl font-bold">
                                     {item.city}
                                 </h1>
