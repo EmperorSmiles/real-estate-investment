4cb864667f53f066c95f66e6d65f0d392a2c1e3d
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-02 12:32:12 +0100

    Updated

--- src/app/components/Steps/layout.tsx
+++ src/app/components/Steps/layout.tsx
@@ -1,3 +1,5 @@
+import ArrowButton from "@/app/components/ArrowButtons/layout";
+
 export default function Steps() {
     const steps = [
         {
@@ -37,12 +39,15 @@ export default function Steps() {
             <h1 className="text-2xl font-bold text-gray-700 font-montserrat mb-4">
                 5 Steps to the First Investment
             </h1>
-            <p className="w-[70vw] text-gray-500 text-sm ">
-                Embarking on your investment journey can be a thrilling and
-                rewarding experience. Let&apos;s walk you through the
-                fundamentals steps to kickstart your investment portfolio with
-                confidence
-            </p>
+            <div className="flex justify-between p-2">
+                <p className="w-[70vw] text-gray-500 text-sm ">
+                    Embarking on your investment journey can be a thrilling and
+                    rewarding experience. Let&apos;s walk you through the
+                    fundamentals steps to kickstart your investment portfolio
+                    with confidence
+                </p>
+                <ArrowButton />
+            </div>
             <div className="flex overflow-clip">
                 {steps.map((text, idx) => (
                     <div key={idx} className=" p-2 text-sm">
