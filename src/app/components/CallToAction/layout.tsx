aff13658861b5a444212ae62b65318ee7fc707c2
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 10:45:35 +0100

    Updated

--- src/app/components/CallToAction/layout.tsx
+++ src/app/components/CallToAction/layout.tsx
@@ -1,7 +1,13 @@
-export default function CallToAction(text: string) {
+interface CallToActionProps {
+    text: string;
+}
+
+const CallToAction: React.FC<CallToActionProps> = ({ text }) => {
     return (
         <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 h-8 lg:m hover:bg-gray-800 hover:text-white w-full lg:w-96 my-8">
             {text}
         </button>
     );
-}
+};
+
+export default CallToAction;
