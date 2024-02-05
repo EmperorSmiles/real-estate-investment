2a0d2a754afd76220bc91ae8578ed5763916c58c
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 10:57:32 +0100

    Updated

--- src/app/components/CallToAction/layout.tsx
+++ src/app/components/CallToAction/layout.tsx
@@ -1,7 +1,12 @@
+// Import React and other necessary dependencies
+import React from "react";
+
+// Define the PropTypes for the CallToAction component
 interface CallToActionProps {
     text: string;
 }
 
+// Function component with TypeScript PropTypes
 const CallToAction: React.FC<CallToActionProps> = ({ text }) => {
     return (
         <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 h-8 lg:m hover:bg-gray-800 hover:text-white w-full lg:w-96 my-8">
@@ -10,4 +15,5 @@ const CallToAction: React.FC<CallToActionProps> = ({ text }) => {
     );
 };
 
+// Export the component
 export default CallToAction;
