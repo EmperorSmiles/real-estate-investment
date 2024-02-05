524dad20439a07e4eaafd5fe33c5720fb96c07eb
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 18:38:18 +0100

    Updated

--- src/app/components/Testimonials/layout.tsx
+++ src/app/components/Testimonials/layout.tsx
@@ -1,26 +1,13 @@
+import { testimonials } from "@/app/utilis/constants";
+
 export default async function layout() {
-    const url = "https://testimonial.p.rapidapi.com/api";
-    const options = {
-        method: "GET",
-        headers: {
-            "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-            "X-RapidAPI-Host": "testimonial.p.rapidapi.com",
-        },
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-
     return (
         <div>
             <div>
                 <h1>Our testimony</h1>
-                <p></p>
+                {testimonials.map((instance, idx) => (
+                    <div key={idx}></div>
+                ))}
             </div>
         </div>
     );
