53332339ed9a0bf2f383310be128dd42030f8884
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 10:40:04 +0100

    Updated

--- src/app/components/CallToAction/layout.tsx
+++ src/app/components/CallToAction/layout.tsx
@@ -1,4 +1,4 @@
-export default function CallToAction({ text }: { text: string }) {
+export default function CallToAction(text: string) {
     return (
         <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 h-8 lg:m hover:bg-gray-800 hover:text-white w-full lg:w-96 my-8">
             {text}
