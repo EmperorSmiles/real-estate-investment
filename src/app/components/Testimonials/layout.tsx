47e6b2aa1444bbece207bbc77ed386804e2cde8d
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 10:19:36 +0100

    Updated

--- src/app/components/Testimonials/layout.tsx
+++ src/app/components/Testimonials/layout.tsx
@@ -1,3 +1,27 @@
-export default function layout() {
-    return <div></div>;
+export default async function layout() {
+    const url = "https://testimonial.p.rapidapi.com/api";
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+            "X-RapidAPI-Host": "testimonial.p.rapidapi.com",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>Our testimony</h1>
+                <p></p>
+            </div>
+        </div>
+    );
 }
