2f9ca84f19c226b03d774a093f2b5224704f0979
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-05 18:42:14 +0100

    Updated

--- src/app/components/Testimonials/layout.tsx
+++ src/app/components/Testimonials/layout.tsx
@@ -6,7 +6,13 @@ export default async function layout() {
             <div>
                 <h1>Our testimony</h1>
                 {testimonials.map((instance, idx) => (
-                    <div key={idx}></div>
+                    <div key={idx}>
+                        <h1>{instance.text}</h1>
+                        <div>{instance.picture}</div>
+                        <div>{instance.name}</div>
+                        <div>{instance.location}</div>
+                        <div>{instance.rating}</div>
+                    </div>
                 ))}
             </div>
         </div>
