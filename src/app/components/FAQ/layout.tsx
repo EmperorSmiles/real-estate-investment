5327a7921755eba48605acc02d0ea602039df4ad
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-09 21:46:47 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -1,13 +1,13 @@
 export default function layout() {
     return (
-        <div className="bg-gray-100 h-full p-8">
+        <div className="bg-gray-100 h-full p-8 grid grid-cols-3">
             <div>Top Questions Answered</div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="col-span-2">
+                <div>Buy Sell & Diversify</div>
+                <div>Buy Sell & Diversify</div>
+                <div>Buy Sell & Diversify</div>
+                <div>Buy Sell & Diversify</div>
+                <div>Buy Sell & Diversify</div>
             </div>
         </div>
     );
