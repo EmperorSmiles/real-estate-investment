c423528930585bdad89343ba62548b851a2c7008
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-09 13:19:27 +0100

    Updated

--- src/app/components/Testimonials/layout.tsx
+++ src/app/components/Testimonials/layout.tsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 export default async function layout() {
     return (
         <div className="bg-slate-200 h-full p-8">
-            <div className="flex gap-6 h-full shrink-0 overflow-auto">
+            <div className="flex gap-6 h-full shrink-0 overflow-auto p-4">
                 {testimonials.map((instance, idx) => (
                     <div
                         key={idx}
-                        className="w-full flex-shrink-0 h-full shadow-md rounded-md py-2"
+                        className="w-full flex-shrink-0 h-full shadow-md rounded-md p-4 mx-4"
                     >
-                        <span className="grid grid-cols-3">
-                            <h1 className="capitalize font-roboto font-bold text-3xl col-span-1">
-                                Our testimony
-                            </h1>
-                            <h1 className="p-2 font-roboto italic text-xl col-span-2">
-                                {instance.text}
-                            </h1>
-                        </span>
+                        <h1 className="capitalize font-roboto font-bold text-3xl">
+                            Our testimony
+                        </h1>
+
+                        <h1 className="p-2 font-roboto italic font-bold text-2xl w-full pl-64 pb-4">
+                            {instance.text}
+                        </h1>
+
                         <div className="flex justify-between">
                             <div className="flex gap-2">
                                 <Image
