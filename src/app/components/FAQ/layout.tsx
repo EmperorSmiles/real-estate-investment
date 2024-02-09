5380610a0bea5292bce46c7cda4ff6d6d564539a
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-09 21:32:56 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -1,6 +1,6 @@
 export default function layout() {
     return (
-        <div>
+        <div className="bg-slate-200 h-full p-8">
             <div>Top Questions Answered</div>
             <div></div>
         </div>
