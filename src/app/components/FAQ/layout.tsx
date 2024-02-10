4968d58ddb434631d02707983add9be9676184d2
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-10 10:10:08 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -2,48 +2,7 @@ export default function layout() {
     return (
         <div className="bg-gray-100 h-full p-8 grid grid-cols-3">
             <div>Top Questions Answered</div>
-            <div className="col-span-2">
-                <div>
-                    <div className="font-bold">Buy Sell & Diversify</div>
-                    <p className="py-8">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quibusdam necessitatibus quam accusamus eligendi,
-                        recusandae soluta. Vero, blanditiis?
-                    </p>
-                </div>
-                <div>
-                    <div>Buy Sell & Diversify</div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quibusdam necessitatibus quam accusamus eligendi,
-                        recusandae soluta. Vero, blanditiis?
-                    </p>
-                </div>
-                <div>
-                    <div>Buy Sell & Diversify</div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quibusdam necessitatibus quam accusamus eligendi,
-                        recusandae soluta. Vero, blanditiis?
-                    </p>
-                </div>
-                <div>
-                    <div>Buy Sell & Diversify</div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quibusdam necessitatibus quam accusamus eligendi,
-                        recusandae soluta. Vero, blanditiis?
-                    </p>
-                </div>
-                <div>
-                    <div>Buy Sell & Diversify</div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quibusdam necessitatibus quam accusamus eligendi,
-                        recusandae soluta. Vero, blanditiis?
-                    </p>
-                </div>
-            </div>
+            <div className="col-span-2"></div>
         </div>
     );
 }
