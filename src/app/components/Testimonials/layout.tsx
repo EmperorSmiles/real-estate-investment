4dd382cdc42491914ca0c640ca4c2993a2fb4bc0
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-10 12:30:10 +0100

    Updated

--- src/app/components/Testimonials/layout.tsx
+++ src/app/components/Testimonials/layout.tsx
@@ -1,14 +1,43 @@
 import { testimonials } from "@/app/utilis/constants";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default async function layout() {
+interface Testimonial {
+    text: string;
+    picture: string;
+    name: string;
+    location: string;
+    rating: any;
+}
+
+interface TestimonialsProps {
+    testimonials: Testimonial[];
+}
+
+export default function Testimonials({ testimonials }: TestimonialsProps) {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(
+                (prevIndex) => (prevIndex + 1) % testimonials.length
+            );
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [testimonials.length]);
+
     return (
         <div className="bg-slate-200 h-full p-8">
             <div className="flex gap-6 h-full shrink-0 overflow-auto p-4">
                 {testimonials.map((instance, idx) => (
                     <div
                         key={idx}
-                        className="w-full flex-shrink-0 h-full shadow-md rounded-md p-4 mx-4"
+                        className={`w-full flex-shrink-0 h-full shadow-md rounded-md p-4 mx-4 transition-transform transform ${
+                            idx === currentIndex
+                                ? "opacity-100"
+                                : "opacity-0 -translate-x-full"
+                        }`}
                     >
                         <h1 className="capitalize font-roboto font-bold text-3xl">
                             Our testimony
