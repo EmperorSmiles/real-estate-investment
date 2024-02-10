5801e27f1abd77c55d112c0bf1e57c995a6b9b4e
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-10 11:51:49 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -20,28 +20,35 @@ const FAQSection: React.FC<FAQSectionProps> = ({ faqs }) => {
         setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
     };
 
+    const isExpanded = (index: number) => expandedIndex === index;
+
     return (
         <div className="bg-gray-100 h-full p-8 grid grid-cols-3">
             <div>Top Questions Answered</div>
             <div className="col-span-2">
                 {faqs.map((content, idx) => (
                     <div key={idx} className="">
-                        <div
-                            className="cursor-pointer"
-                            onClick={() => handleToggle(idx)}
-                        >
-                            <div className="font-bold">{content.topic}</div>
-                            <button>
-                                <FaPlus />
-                            </button>
-                            <button>
-                                <FaMinus />
-                            </button>
+                        <div className="cursor-pointer">
+                            <div className="flex items-center justify-between py-4 align-middle border-b-4">
+                                <div className="font-bold">{content.topic}</div>
+                                <button
+                                    onClick={() => handleToggle(idx)}
+                                    className={`ml-2 p-2 rounded-full ${
+                                        isExpanded(idx)
+                                            ? "bg-black text-white"
+                                            : "bg-gray-300 text-black"
+                                    }`}
+                                >
+                                    {isExpanded(idx) ? <FaMinus /> : <FaPlus />}
+                                </button>
+                            </div>
                         </div>
-                        {expandedIndex === idx && (
-                            <div className="py-8 transition-all">
-                                <p>{content.answer}</p>
-                                <p>{content.p2}</p>
+                        {isExpanded(idx) && (
+                            <div className="py-8 transition-all duration-5000 ease-linear">
+                                <p className="mb-4 opacity-100">
+                                    {content.answer}
+                                </p>
+                                <p className="opacity-80">{content.p2}</p>
                             </div>
                         )}
                     </div>
