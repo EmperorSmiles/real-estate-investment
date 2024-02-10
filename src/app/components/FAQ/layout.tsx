f9bc22cef3904137006c2d58b339cac7802d279c
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-10 09:53:06 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -3,16 +3,46 @@ export default function layout() {
         <div className="bg-gray-100 h-full p-8 grid grid-cols-3">
             <div>Top Questions Answered</div>
             <div className="col-span-2">
-                <div>Buy Sell & Diversify</div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Quibusdam necessitatibus quam accusamus eligendi, recusandae
-                    soluta. Vero, blanditiis?
-                </p>
-                <div>Buy Sell & Diversify</div>
-                <div>Buy Sell & Diversify</div>
-                <div>Buy Sell & Diversify</div>
-                <div>Buy Sell & Diversify</div>
+                <div>
+                    <div>Buy Sell & Diversify</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Quibusdam necessitatibus quam accusamus eligendi,
+                        recusandae soluta. Vero, blanditiis?
+                    </p>
+                </div>
+                <div>
+                    <div>Buy Sell & Diversify</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Quibusdam necessitatibus quam accusamus eligendi,
+                        recusandae soluta. Vero, blanditiis?
+                    </p>
+                </div>
+                <div>
+                    <div>Buy Sell & Diversify</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Quibusdam necessitatibus quam accusamus eligendi,
+                        recusandae soluta. Vero, blanditiis?
+                    </p>
+                </div>
+                <div>
+                    <div>Buy Sell & Diversify</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Quibusdam necessitatibus quam accusamus eligendi,
+                        recusandae soluta. Vero, blanditiis?
+                    </p>
+                </div>
+                <div>
+                    <div>Buy Sell & Diversify</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Quibusdam necessitatibus quam accusamus eligendi,
+                        recusandae soluta. Vero, blanditiis?
+                    </p>
+                </div>
             </div>
         </div>
     );
