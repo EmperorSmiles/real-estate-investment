c4e072a6de9be69414e250e20b475036c14ca856
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-10 10:41:27 +0100

    Updated

--- src/app/components/FAQ/layout.tsx
+++ src/app/components/FAQ/layout.tsx
@@ -1,22 +1,47 @@
+"use client";
+import React, { useState } from "react";
 import { faqs } from "@/app/utilis/constants";
 
-export default function layout() {
+interface FAQ {
+    topic: string;
+    answer: string;
+    p2: string;
+}
+
+interface FAQSectionProps {
+    faqs: FAQ[];
+}
+
+const FAQSection: React.FC<FAQSectionProps> = ({ faqs }) => {
+    const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+    const handleToggle = (index: number) => {
+        setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
+    };
+
     return (
         <div className="bg-gray-100 h-full p-8 grid grid-cols-3">
             <div>Top Questions Answered</div>
             <div className="col-span-2">
                 {faqs.map((content, idx) => (
                     <div key={idx}>
-                        <div>
+                        <div
+                            className="cursor-pointer"
+                            onClick={() => handleToggle(idx)}
+                        >
                             <div className="font-bold">{content.topic}</div>
-                            <div className="py-8">
+                        </div>
+                        {expandedIndex === idx && (
+                            <div className="py-8 transition-all">
                                 <p>{content.answer}</p>
                                 <p>{content.p2}</p>
                             </div>
-                        </div>
+                        )}
                     </div>
                 ))}
             </div>
         </div>
     );
-}
+};
+
+export default FAQSection;
