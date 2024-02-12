2d019898d2e8acc998ec1d2123b3c294fed0717b
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-12 11:08:42 +0100

    Moved components out of app folder

---
+++ components/NavBar/layout.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <nav className="bg-transparent text-gray-300 font-open_sans absolute inset-x-0 top-0 z-30">
+            <div className="flex justify-around p-3 items-center ">
+                <h1 className="text-lg font-bold">
+                    Intelli
+                    <span className="text-indigo-500 font-extrabold">V</span>est
+                </h1>
+                <ul className="flex space-x-3 text-sm ">
+                    <li className="hover:text-indigo-500">
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className="hover:text-indigo-500">
+                        <Link href="header">How it Works</Link>
+                    </li>
+                    <li className="hover:text-indigo-500">
+                        <Link href="#">Smart Property Investing</Link>
+                    </li>
+                    <li className="hover:text-indigo-500">
+                        <Link href="#">Market Insights</Link>
+                    </li>
+                    <li className="hover:text-indigo-500">
+                        <Link href="#">Market Insights</Link>
+                    </li>
+                    <li className="hover:text-indigo-500">
+                        <Link href="#">FAQs</Link>
+                    </li>
+                </ul>
+                <button className="bg-transparent rounded-3xl border-gray-300 border-2 px-2 hover:border-indigo-500 hover:text-indigo-500">
+                    Get Started
+                </button>
+            </div>
+        </nav>
+    );
+}
