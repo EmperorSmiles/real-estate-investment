4db8d2172580b2c9f249b60b561b26201f3eaa7f
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-12 11:10:06 +0100

    Moved components out of app folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar/layout";
+// import NavBar from "./components/NavBar/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <NavBar />
+                {/* <NavBar /> */}
                 {children}
             </body>
         </html>
