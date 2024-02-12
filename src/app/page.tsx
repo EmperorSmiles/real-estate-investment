0f8b16c480825ce0a0dee77bcef41699fb02ad91
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-12 11:10:14 +0100

    Moved components out of app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import NavBar from "./components/NavBar/layout";
-import Hero from "./components/Hero/layout";
-import About from "./components/About/layout";
-import Body from "./components/Body/layout";
-import Property from "./components/LatestProperty/layout";
-import Example from "./components/Example/layout";
-import Steps from "./components/Steps/layout";
-import Testimonials from "./components/Testimonials/layout";
-import FAQSection from "./components/FAQ/layout";
+import NavBar from "@/components/NavBar/layout";
+import Hero from "@/components/Hero/layout";
+import About from "@/components/About/layout";
+import Body from "@/components/Body/layout";
+import Property from "@/components/LatestProperty/layout";
+import Example from "@/components/Example/layout";
+import Steps from "@/components/Steps/layout";
+import Testimonials from "@/components/Testimonials/layout";
+import FAQSection from "@/components/FAQ/layout";
 import { faqs, testimonials } from "./utilis/constants";
 import { ContextProvider } from "@/app/context/Context";
 
