156d35bf80948a224ccea886d08a1915c686f667
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-18 17:48:16 +0100

    Updated

--- src/components/Steps/layout.tsx
+++ src/components/Steps/layout.tsx
@@ -4,6 +4,7 @@ import ArrowButton from "../../components/ArrowButtons/layout";
 import { steps } from "@/app/utilis/constants";
 import { useLatestProperty } from "@/app/context/Context";
 import { useEffect } from "react";
+import Slider from "react-slick";
 
 export default function Steps() {
     const { activeImage, currentStepIndex, nextStep, prevStep } =
@@ -70,6 +71,29 @@ export default function Steps() {
                     </div>
                 ))}
             </div> */}
+            <div className="w-full h-full overflow-hidden p-4 my-4 bg-gray-100 ">
+                <Slider className="bg-orangre-500">
+                    {steps.map((text, idx) => (
+                        <div key={idx} className="p-2 text-sm">
+                            <div
+                                key={idx}
+                                className="flex-col h-64 p-4 shadow-md justify-between w-80 flex-shrink-0"
+                            >
+                                <h1 className="font-bold text-lg font-montserrat pb-3">
+                                    {text.title}
+                                </h1>
+                                <h1 className="font-bold text-lg font-montserrat capitalize pb-2">
+                                    {text.desc}
+                                </h1>
+                                <div className="flex-grow flex flex-col justify-center pb-4">
+                                    <p className="text-gray-500">{text.note}</p>
+                                </div>
+                                <p className="text-gray-500">{text.note2}</p>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+            </div>
         </div>
     );
 }
