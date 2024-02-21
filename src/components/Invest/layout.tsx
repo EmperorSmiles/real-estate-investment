150e775b9d1cb33c2c8b5cdc2cbd981762712ad8
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-21 13:57:54 +0100

    Updated

--- src/components/Invest/layout.tsx
+++ src/components/Invest/layout.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function layout() {
     return (
-        <div className="relative bg-cover bg-center h-96">
+        <div className="relative bg-cover bg-center h-96 flex items-center justify-center">
             <Image
                 src={background}
                 alt="Hero"
@@ -14,17 +14,20 @@ export default function layout() {
                 priority
                 className="object-cover h-full w-full"
             />
-            <div className="absolute inset-0 bg-gradient-to-b from-gray-200 to-gray-200 opacity-70"></div>
-            <div className="absolute inset-0 top-14 left-56">
-                <p className="font-montserrat">Unlock Your Dream Home Today!</p>
-                <p>
+            <div className="absolute inset-0 bg-gradient-to-b from-gray-200 to-gray-200 opacity-80"></div>
+            <div className="absolute inset-0 top-14 left-24 flex flex-col items-center justify-center text-center px-4">
+                <p className="font-montserrat text-5xl text-center">
+                    Unlock Your Dream Home Today!
+                </p>
+                <p className="text-xl text-center mt-6">
                     Join us on the journey to home ownership and discover the
                     perfect place to call your own.
                 </p>
-                <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 text-xs lg:text-sm h-8 lg:m hover:bg-gray-800 hover:text-white w-60 lg:w-56 my-4 lg:my-8">
+                <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 text-xs lg:text-sm h-8 hover:bg-gray-800 hover:text-white lg:w-56 my-4 lg:my-8 ">
                     Find Out More
                 </button>
             </div>
+            <div className="absolute bottom-0">INTELLIVEST</div>
         </div>
     );
 }
