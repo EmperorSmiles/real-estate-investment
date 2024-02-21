2844e47610f67cfab7f64453b0160320bb64e6e2
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-21 12:43:52 +0100

    Updated

--- src/components/Invest/layout.tsx
+++ src/components/Invest/layout.tsx
@@ -1,19 +1,5 @@
-import Image from "next/image";
-import background from "../../../public/BGimage.png";
+import React from "react";
 
 export default function layout() {
-    return (
-        <div className="relative bg-cover bg-center h-96">
-            <Image
-                src={background}
-                alt="Hero"
-                height={900}
-                width={500}
-                quality={100}
-                priority
-                className="object-cover h-full w-full"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-black to-black opacity-70"></div>
-        </div>
-    );
+    return <div className="bg-black text-white">Hello!</div>;
 }
