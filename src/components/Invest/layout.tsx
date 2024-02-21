ff8857048997f49d531caa208e919b8eec7cd06d
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-21 12:08:30 +0100

    Updated

--- src/components/Invest/layout.tsx
+++ src/components/Invest/layout.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import background from "../../../public/BGimage.png";
+
 export default function layout() {
-    return <div></div>;
+    return (
+        <div className="relative bg-cover bg-center h-96">
+            <Image
+                src={background}
+                alt="Hero"
+                height={900}
+                width={500}
+                quality={100}
+                priority
+                className="object-cover h-full w-full"
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-black to-black opacity-70"></div>
+        </div>
+    );
 }
