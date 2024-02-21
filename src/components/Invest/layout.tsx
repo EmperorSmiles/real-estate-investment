7ab2bb9ad0ea0ac4e99777cbefafec5e0baa1cda
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-21 13:14:24 +0100

    Updated

--- src/components/Invest/layout.tsx
+++ src/components/Invest/layout.tsx
@@ -15,7 +15,7 @@ export default function layout() {
                 className="object-cover h-full w-full"
             />
             <div className="absolute inset-0 bg-gradient-to-b from-gray-200 to-gray-200 opacity-70"></div>
-            <div className="absolute inset-0 top-6">
+            <div className="absolute inset-0 top-14 left-56">
                 <p>Unlock Your Dream Home Today!</p>
                 <p>
                     Join us on the journey to home ownership and discover the
