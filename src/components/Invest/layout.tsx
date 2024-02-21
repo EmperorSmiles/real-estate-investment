4e4eee4f230d0526660904b781bde1f3ea800614
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-21 13:12:56 +0100

    Updated

--- src/components/Invest/layout.tsx
+++ src/components/Invest/layout.tsx
@@ -15,12 +15,15 @@ export default function layout() {
                 className="object-cover h-full w-full"
             />
             <div className="absolute inset-0 bg-gradient-to-b from-gray-200 to-gray-200 opacity-70"></div>
-            <div className="absolute inset-0">
+            <div className="absolute inset-0 top-6">
                 <p>Unlock Your Dream Home Today!</p>
                 <p>
                     Join us on the journey to home ownership and discover the
                     perfect place to call your own.
                 </p>
+                <button className="bg-transparent rounded-xl border-2 border-gray-700 text-gray-700 text-xs lg:text-sm h-8 lg:m hover:bg-gray-800 hover:text-white w-60 lg:w-56 my-4 lg:my-8">
+                    Find Out More
+                </button>
             </div>
         </div>
     );
