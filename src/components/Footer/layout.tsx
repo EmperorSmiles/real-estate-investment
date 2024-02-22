793aec436d009c83fdb46d149a1db056c1035a0e
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 12:23:46 +0100

    Updated

--- src/components/Footer/layout.tsx
+++ src/components/Footer/layout.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Footer() {
-    return <div className="bg-gray-100 h-full">FYM</div>;
+    return (
+        <div className="bg-gray-100 h-full flex flex-col">
+            <div>FYM</div>
+            <div>Copyright</div>
+        </div>
+    );
 }
