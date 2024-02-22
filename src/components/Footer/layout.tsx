de80fcf6ca96f2bf6647ebcc84a82eaeff3964d3
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 12:36:40 +0100

    Updated

--- src/components/Footer/layout.tsx
+++ src/components/Footer/layout.tsx
@@ -2,25 +2,25 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="bg-gray-100 h-full flex flex-col">
+        <div className="bg-gray-100 h-full flex flex-col p-8">
             <div className="flex justify-between">
                 <ul>
-                    <li>FEATURES</li>
+                    <li className="font-bold">FEATURES</li>
                     <li>How it Works</li>
                     <li>For Teams</li>
                     <li>Template</li>
                 </ul>
                 <ul>
-                    <li>COMPANY</li>
+                    <li className="font-bold">COMPANY</li>
                     <li>About Us</li>
                     <li>Careers</li>
                     <li>Blog</li>
                 </ul>
                 <ul>
-                    <li>FEATURES</li>
-                    <li>How it Works</li>
-                    <li>For Teams</li>
-                    <li>Template</li>
+                    <li className="font-bold">RESOURCES</li>
+                    <li>Download Apps</li>
+                    <li>Help Center</li>
+                    <li>Productivity Methods</li>
                 </ul>
                 <ul>
                     <li>FEATURES</li>
