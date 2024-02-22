81a09d6a6543ca8067aa67d7959f2d7e05507830
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 12:29:47 +0100

    Updated

--- src/components/Footer/layout.tsx
+++ src/components/Footer/layout.tsx
@@ -3,18 +3,25 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="bg-gray-100 h-full flex flex-col">
-            <div>
-                <div>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="flex">
+                <ul>
+                    <li>FEATURES</li>
+                    <li>How it Works</li>
+                    <li>For Teams</li>
+                    <li>Template</li>
+                </ul>
+                <ul>
+                    <li>FEATURES</li>
+                    <li>How it Works</li>
+                    <li>For Teams</li>
+                    <li>Template</li>
+                </ul>
+                <ul>
+                    <li>FEATURES</li>
+                    <li>How it Works</li>
+                    <li>For Teams</li>
+                    <li>Template</li>
+                </ul>
             </div>
             <div className="bg-slate-200">Copyright</div>
         </div>
