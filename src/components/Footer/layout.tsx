b02d32368148f2c07309b0d2642d117f88cdc8cf
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 19:35:36 +0100

    Updated

--- src/components/Footer/layout.tsx
+++ src/components/Footer/layout.tsx
@@ -45,7 +45,7 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="bg-slate-200">
+            <div className="bg-slate-200 text-center p-6">
                 Intellivest &copy; 2024, All rights reserved by
                 <Link
                     className="underline ml-2"
