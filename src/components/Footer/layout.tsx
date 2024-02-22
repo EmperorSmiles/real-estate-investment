e2aae4bcc0e072cd5addbf0e54ed7a544b54809d
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 12:26:56 +0100

    Updated

--- src/components/Footer/layout.tsx
+++ src/components/Footer/layout.tsx
@@ -3,7 +3,19 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="bg-gray-100 h-full flex flex-col">
-            <div>FYM</div>
+            <div>
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
             <div className="bg-slate-200">Copyright</div>
         </div>
     );
