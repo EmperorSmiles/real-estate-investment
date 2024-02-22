983582b90b7867eb6a8e79dba6758915d6e0efa9
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-22 22:13:30 +0100

    Updated

--- src/components/NavBar/layout.tsx
+++ src/components/NavBar/layout.tsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function NavBar() {
     const [drawerOpen, setDrawerOpen] = useState(false);
@@ -25,9 +26,12 @@ export default function NavBar() {
         setDrawerOpen(!drawerOpen);
     };
 
+    const closeDrawer = () => {
+        setDrawerOpen(false);
+    };
+
     return (
         <nav
-            // className={`bg-transparent text-gray-300 font-open_sans absolute inset-x-0 top-0 z-30`}
             className={` ${
                 scrolled
                     ? "fixed top-0 left-0 w-full z-30 bg-gray-800 shadow-md transition-all duration-500 font-open_sans text-gray-300 shadow-gray-800"
@@ -68,57 +72,71 @@ export default function NavBar() {
                 </button>
             </div>
             {drawerOpen && (
-                <div className="lg:hidden fixed inset-0 bg-gray-800 z-40">
-                    <div className="flex justify-end p-4">
-                        <FaTimes
-                            onClick={toggleDrawer}
-                            className="text-white text-2xl cursor-pointer"
-                        />
-                    </div>
-                    <div className="flex flex-col items-center p-4">
-                        <Link
-                            href="/"
-                            onClick={toggleDrawer}
-                            className="hover:text-indigo-500"
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            href="#"
-                            onClick={toggleDrawer}
-                            className="hover:text-indigo-500"
-                        >
-                            How it Works
-                        </Link>
-                        <Link
-                            href="#"
-                            onClick={toggleDrawer}
-                            className="hover:text-indigo-500"
-                        >
-                            Smart Property Investing
-                        </Link>
-                        <Link
-                            href="#"
-                            onClick={toggleDrawer}
-                            className="hover:text-indigo-500"
-                        >
-                            Market Insights
-                        </Link>
-                        <Link
-                            href="#"
-                            onClick={toggleDrawer}
-                            className="hover:text-indigo-500"
-                        >
-                            FAQs
-                        </Link>
-                        <button
-                            className="bg-transparent rounded-3xl border-gray-300 border-2 px-2 mt-4 hover:border-indigo-500 hover:text-indigo-500"
-                            onClick={toggleDrawer}
-                        >
-                            Get Started
-                        </button>
-                    </div>
-                </div>
+                <AnimatePresence>
+                    <motion.div
+                        key="drawer"
+                        className="lg:hidden fixed inset-0 bg-gray-800 z-40"
+                        initial={{ width: 1 }}
+                        animate={{ width: "100%" }}
+                        exit={{
+                            x: "-100%",
+                        }}
+                        transition={{ type: "tween", duration: 0.5 }}
+                    >
+                        <div className="flex justify-end p-4">
+                            <motion.div
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, scale: 0.8, x: "-100%" }}
+                                transition={{ duration: 0.3, delay: 0.5 }}
+                            >
+                                <FaTimes
+                                    onClick={closeDrawer}
+                                    className="text-white text-2xl cursor-pointer"
+                                />
+                            </motion.div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            {[
+                                { href: "/", text: "Home" },
+                                { href: "#", text: "How it Works" },
+                                { href: "#", text: "Smart Property Investing" },
+                                { href: "#", text: "Market Insights" },
+                                { href: "#", text: "FAQs" },
+                            ].map((link, index) => (
+                                <motion.div
+                                    key={index}
+                                    initial={{ opacity: 0, y: 20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: 20 }}
+                                    transition={{
+                                        duration: 0.3,
+                                        delay: index * 0.1,
+                                    }}
+                                >
+                                    <Link
+                                        href={link.href}
+                                        onClick={toggleDrawer}
+                                        className="hover:text-indigo-500"
+                                    >
+                                        {link.text}
+                                    </Link>
+                                </motion.div>
+                            ))}
+                            <motion.button
+                                key="button"
+                                className="bg-transparent rounded-3xl border-gray-300 border-2 px-2 mt-4 hover:border-indigo-500 hover:text-indigo-500"
+                                onClick={toggleDrawer}
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 20 }}
+                                transition={{ duration: 0.3, delay: 0.5 }}
+                            >
+                                Get Started
+                            </motion.button>
+                        </div>
+                    </motion.div>
+                </AnimatePresence>
             )}
         </nav>
     );
