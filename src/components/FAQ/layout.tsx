e6c97555c36cb30cd5dd4397424ab833b3dbb724
EmperorSmiles <iamfavoursmiles@gmail.com>
2024-02-27 11:20:02 +0100

    Updated

--- src/components/FAQ/layout.tsx
+++ src/components/FAQ/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { faqs } from "@/app/utilis/constants";
+// import { faqs } from "@/app/utilis/constants";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 interface FAQ {
